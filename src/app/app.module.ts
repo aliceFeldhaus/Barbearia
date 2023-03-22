7d6da32dcb5b2a1751bc047a4fe29a3a65104638
aliceFeldhaus <alicesfeldhaus@gmail.com>
2023-03-21 22:50:35 -0300

    Projeto front Angular barbearia

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { AgendarComponent } from './agendar/agendar.component';
+import { MeusAgendamentosComponent } from './meus-agendamentos/meus-agendamentos.component';
+import { BarraNavegacaoComponent } from './barra-navegacao/barra-navegacao.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    AgendarComponent,
+    MeusAgendamentosComponent,
+    BarraNavegacaoComponent
   ],
   imports: [
     BrowserModule,
